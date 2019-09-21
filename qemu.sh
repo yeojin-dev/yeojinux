fb313271d280fdb030d87f66d55334560d06a434
Yeojin Kim <yeojin.dev@gmail.com>
2019-09-22 07:17:36 +0900

    Fix bug

--- qemu.sh
+++ qemu.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64
-    -m 64  # 64M 메모리 할당
-    -fda /Users/$1/projects/yeojinux/Disk.img  # 플로피 디스크 이미지 지정
-    -rtc base=localtime  # 시간 설정
-    -M pc  # 일반 PC 환경으로 설정
+qemu-system-x86_64 -m 64 -fda /Users/$1/projects/yeojinux/Disk.img -rtc base=localtime -M pc
