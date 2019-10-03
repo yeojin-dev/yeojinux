67c3cc376af0a7e2751954d21deeb88f095418a0
Yeojin Kim <yeojin.dev@gmail.com>
2019-10-03 23:30:25 +0900

    Add code for loading OS image

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -5,11 +5,22 @@ SECTION .txt                ; text 섹션(세그먼트) 정의
 
 jmp 0x07C0:START            ; CS 세그먼트 레지스터에 0x07C0을 복사하면서, START 레이블로 이동
 
+; MINT64 OS에 관련된 환경 설정 값
+TOTALSECTORCOUNT:   dw 1024 ; 부트 로더를 제외한 MINT64 OS 이미지의 크기, 최대 1152섹터(0x90000바이트)까지 가능
+
+; 코드 영역
 START:
     mov ax, 0x07C0          ; 부트 로더의 시작 어드레스(0x7C00)를 세그먼트 레지스터 값으로 변환
     mov ds, ax              ; DS 세그먼트 레지스터에 설정
     mov ax, 0xB800          ; 비디오 메모리의 시작 어드레스(0xB800)를 세그먼트 레지스터 값으로 변환
     mov es, ax              ; ES 세그먼트 레지스터에 설정
+
+    ;스택을 0x0000:0000~0x0000:FFFF 영역에 64KB 크기로 생성
+    mov ax, 0x0000          ; 스택 세그먼트의 시작 어드레스(0x0000)를 세그먼트 레지스터 값으로 변환
+    mov ss, ax              ; SS 세그먼트 레지스터에 저장
+    mov sp, 0xFFFE          ; SP 레지스터의 어드레스를 0xFFFE로 설정
+    mov bp, 0xFFFE          ; BP 레지스터의 어드레스를 0xFFFE로 설정
+
     mov si, 0               ; SI 레지스터(문자열 원본 인덱스 레지스터)를 초기화
 
 .SCREENCLEARLOOP:               ; 화면을 지우는 루프
@@ -18,11 +29,121 @@ START:
     add si, 2                   ; 문자와 속성을 설정했으므로 다음 위치로 이동
     cmp si, 80 * 25 * 2         ; 화면 전체 크기는 80문자 * 25라인, 출력한 문자의 수를 의미하는 SI 레지스터와 비교
     jl .SCREENCLEARLOOP         ; SI 레지스터가 80 * 25 * 2보다 작다면 아직 지우지 못 한 영역이 있으므로 .SCREENCLEARLOOP 레이블로 이동
-    mov si, 0                   ; SI 레지스터(문자열 원본 인덱스 레지스터)를 초기화
-    mov di, 0                   ; DI 레지스터(문자열 대상 인덱스 레지스터)를 초기화
+
+    ; 화면 상단에 시작 메시지 출력
+    push MESSAGE1               ; 출력할 메시지의 어드레스를 스택에 삽입
+    push 0                      ; 화면 Y 좌표를 스택에 삽입
+    push 0                      ; 화면 X 좌표를 스택에 삽입
+    call PRINTMESSAGE           ; PRINTMESSAGE 함수 호출
+    add sp, 6                   ; 삽입한 파라미터 제거
+
+    ; OS 이미지를 로딩한다는 메시지 출력
+    push IMAGELOADINGMESSAGE    ; 출력할 메시지의 어드레스를 스택에 삽입
+    push 1                      ; 화면 Y 좌표를 스택에 삽입
+    push 0                      ; 화면 X 좌표를 스택에 삽입
+    call PRINTMESSAGE           ; PRINTMESSAGE 함수 호출
+    add sp, 6                   ; 삽입한 파라미터 제거
+
+    ; 디스크에서 OS 이미지 로딩
+    ; 디스크 읽기 전에 먼저 리셋
+
+RESETDISK:                      ; 디스크 리셋 코드 시작
+    ; BIOS Reset Function 호출
+    ; 서비스 번호 0, 드라이브 번호 0(Floppy)
+    mov ax, 0
+    mov dl, 0
+    int 0x13
+    jc HANDLEDISKERROR          ; 에러가 발생하면 에러 처리로 이동
+
+    ; 디스크에서 섹터를 읽음
+    ; 디스크의 내용을 메모리로 복사할 어드레스(ES:BX)를 0x10000으로 설정
+    mov si, 0x1000              ; OS 이미지를 복사할 어드레스(0x10000)를 세그먼트 레지스터 값으로 변환
+    mov es, si
+    mov bx, 0x0000              ; BX 레지스터에 0x0000을 설정하여 복사할 어드레스를 0x1000:0000(0x10000)으로 최종 설정
+    mov di, word [ TOTALSECTORCOUNT ]   ; 복사할 OS 이미지의 섹터 수를 DI 레지스터에 설정
+
+READDATA:                       ; 디스크를 읽는 코드의 시작
+    ; 모든 섹터를 다 읽었는지 확인
+    cmp di, 0                   ; 복사할 OS 이미지의 섹터 수를 0과 비교
+    je READEND                  ; 복사할 섹터 수가 0이라면 다 복사했으므로 READEND로 이동
+    sub di, 0x1                 ; 복사할 섹터 수 1 감소
+
+    ; BIOS Read Function 호출
+    mov ah, 0x02                    ; BIOS 서비스 번호 2(Read Sector)
+    mov al, 0x1                     ; 읽을 섹터 수는 1
+    mov ch, byte [ TRACKNUMBER ]    ; 읽을 트랙 번호 설정
+    mov cl, byte [ SECTORNUMBER ]   ; 읽을 섹터 번호 설정
+    mov dh, byte [ HEADNUMBER ]     ; 읽을 헤드 번호 설정
+    mov dl, 0x00                    ; 읽을 드라이브 번호 0(Floppy) 설정
+    int 0x13                        ; 인터럽트 서비스 수행
+    jc HANDLEDISKERROR              ; 에러가 발생했다면 HANDLEDISKERROR로 이동
+
+    ; 복사할 어드레스와 트랙, 헤드, 섹터 어드레스 계산
+    ; 읽은 섹터는 ES:BX 위치에 저장됨
+    add si, 0x0020                  ; 512(0x200)바이트만큼 읽었으므로, 이를 세그먼트 레지스터 값으로 변환
+    mov es, si                      ; ES 세그먼트 레지스터에 더해서 어드레스를 한 섹터만큼 증가
+
+    ; 한 섹터를 읽었으므로 섹터 번호를 증가시키고 마지막 섹터(18)까지 읽었는지 판단, 마지막 섹터가 아니면 섹터 읽기로 이동해서 다시 섹터 읽기 수행
+    mov al, byte [ SECTORNUMBER ]   ; 섹터 번호를 AL 레지스터에 설정
+    add al, 0x1                     ; 섹터 번호를 1 증가
+    mov byte [ SECTORNUMBER ], al   ; 증가시킨 섹터 번호를 SECTORNUMBER에 다시 설정
+    cmp al, 19                      ; 증가시킨 섹터 번호를 19와 비교
+    jl READDATA                     ; 섹터 번호가 19 미만이라면 READDATA로 이동
+
+    ; 마지막 섹터까지 읽었으면(섹터 번호가 19이면) 헤드를 토글하고 섹터 번호를 1로 설정
+    xor byte[ HEADNUMBER ], 0x01    ; 헤드 번호를 0x01과 XOR 하여 토글
+    mov byte[ SECTORNUMBER ], 0x01  ; 섹터 번호를 다시 1로 설정
+
+    ; 만약 헤드가 1 -> 0으로 바뀌었으면 양쪽 헤드를 모두 읽은 것이므로 아래로 이동하여 트랙 번호를 1 증가
+    cmp byte[ HEADNUMBER ], 0x00    ; 헤드 번호를 0x00과 비교
+    jne READDATA                    ; 헤드 번호가 0이 아니면 READDATA로 이동
+
+    ; 트랙을 1 증가시킨 후 다시 섹터 읽기로 이동
+    add byte [ TRACKNUMBER ], 0x01  ; 트랙 번호를 1 증가
+    jmp READDATA
+
+READEND:
+    ; OS 이미지가 완료되었다는 메시지를 출력
+    push LOADINGCOMPLETEMESSAGE     ; 출력할 메시지의 어드레스를 스택에 삽입
+    push 2
+    push 20
+    call PRINTMESSAGE
+    add sp, 6
+    jmp 0x1000:0x0000               ; 로딩한 가상 OS 이미지 실행
+
+; 함수 코드 영역
+HANDLEDISKERROR:                ; 디스크 에러를 처리하는 함수
+    push DISKERRORMESSAGE       ; 에러 문자열의 어드레스를 스택에 삽입
+    push 1
+    push 20
+    call PRINTMESSAGE
+    jmp $                       ; 현재 위치에서 무한 루프 수행
+
+PRINTMESSAGE:
+    push bp                     ; 베이스 포인터 레지스터를 스택에 삽입
+    mov bp, sp                  ; 베이스 포인터 레지스터에 스택 포인터 레지스터의 값을 설정
+    push es
+    push si
+    push di
+    push ax
+    push cx
+    push dx
+
+    mov ax, 0xB800              ; 비디오 메모리 시작 어드레스(0x0B8000)를 세그먼트 레지스터 값으로 변환
+    mov es, ax                  ; ES 세그먼트 레지스터에 설정
+
+    ; X, Y의 좌표로 비디오 메모리 어드레스를 계산함
+    ; Y 좌표를 이용해서 먼저 라인 어드레스를 계산함
+    mov ax, word [ bp + 6 ]     ; 파라미터 2(화면 좌표 Y)를 AX 레지스터에 설정
+    mov si, 160                 ; 한 라인의 바이트 수(2 * 80)를 SI 레지스터에 설정
+    mul si                      ; AX 레지스터와 SI 레지스터를 곱하여 화면 X 어드레스를 계산
+    add di, ax                  ; 화면 Y 어드레스와 계산된 X 어드레스를 더해서 실제 비디오 메모리 어드레스를 계산
+
+    ; 출력할 문자열의 어드레스
+    mov si, word [ bp + 8 ]     ; 파라미터 3
 
 .MESSAGELOOP:                   ; 메시지를 출력하는 루프
-    mov cl, byte[si + MESSAGE1] ; MESSAGE1의 어드레스에서 SI 레지스터 값만큼 더한 위치의 문자를 CL 레지스터(CX 레지스터의 하위 1바이트)에 복사
+    mov cl, byte[ si ]          ; SI 레지스터가 가리키는 문자열 위치에서 한 문자를 CL 레지스터(CX 레지스터의 하위 1바이트)에 복사
     cmp cl, 0                   ; 복사된 문자와 0을 비교
     je .MESSAGEEND              ; 복사한 문자의 값이 0이면 문자열이 종료되었음을 의미하므로 문자 출력 종료
     mov byte[es:di], cl         ; 0이 아니라면 비디오 메모리 어드레스 0xB800:di에 문자를 출력
@@ -31,9 +152,26 @@ START:
     jmp .MESSAGELOOP            ; 메시지 출력 루프로 이동하여 다음 문자를 출력
 
 .MESSAGEEND:
-    jmp $                   ; 현재 위치에서 무한 루프 수행
+    pop dx
+    pop cx
+    pop ax
+    pop di
+    pop si
+    pop es
+    pop bp                      ; 베이스 포인터 레지스터 복원
+    ret                         ; 함수를 호출한 다음 코드의 위치로 복귀
 
-MESSAGE1:   db 'MINT64 OS Boot Loader Start~!!', 0  ; 출력할 메시지 정의
+; 데이터 영역
+MESSAGE1:               db 'MINT64 OS Boot Loader Start~!!', 0  ; 출력할 메시지 정의, 마지막은 0으로 설정해서 문자열이 종료되었음을 알 수 있도록 함
+
+DISKERRORMESSAGE:       db 'DISK Error~!!', 0
+IMAGELOADINGMESSAGE:    db 'OS Image Loading...', 0
+LOADINGCOMPLETEMESSAGE: db 'Complete~!!', 0
+
+; 디스크 읽기 관련 변수
+SECTORNUMBER:           db 0x02 ; OS 이미지가 시작하는 섹터 번호를 저장하는 영역
+HEADNUMBER:             db 0x00
+TRACKNUMBER:            db 0x00
 
 times 510 - ($ - $$) db 0x00; $: 현재 라인의 어드레스, $$: 현재 섹션(.text)의 시작 어드레스, $ - $$: 현재 섹션을 기준으로 하는 오프셋
                             ; 510-($-$$): 현재부터 510 어드레스까지, db 0x00: 1바이트를 선언하고 값은 0x00, time: 반복 수행
